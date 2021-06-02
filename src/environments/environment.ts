73e34d7a9ac4bcd3ee2d0822578bd561f971c99c
Marcos Rezende <marcosvrs.dev@gmail.com>
2021-06-01 23:02:43 -0300

    fix: update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
+  production: false,
   upabase_url: 'https://zmgmepgjaulbfsqbmduk.supabase.co',
   supabase_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyMjU5NjU1NCwiZXhwIjoxOTM4MTcyNTU0fQ.jmiuLhueuBaR_CSTc2aRDObHR_dp6AceAV84PeHFLsc',
 };
