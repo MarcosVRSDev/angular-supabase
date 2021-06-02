5793a4411d80a234dc0e72b0466aa0e5fa82dbc2
Marcos Rezende <marcosvrs.dev@gmail.com>
2021-06-01 23:20:36 -0300

    feat: add signin function test

--- src/app/pages/sign-in/sign-in.component.ts
+++ src/app/pages/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { ApiService } from 'src/app/services/api.service';
+
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit(): void {
   }
 
+  signIn() {
+    this.api.singIn();
+  }
 }
